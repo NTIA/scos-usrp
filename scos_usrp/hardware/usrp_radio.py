cd1a59469424861a5f2be96919ea4a325a53a0d4
max <max.hollingsworth@ntia.gov>
2020-08-06 15:44:39 -0600

    debugging added

--- scos_usrp/hardware/usrp_radio.py
+++ scos_usrp/hardware/usrp_radio.py
@@ -650,14 +650,17 @@ class USRPRadio(RadioInterface):
         tx_md = self.uhd.types.TXMetadata()
         tx_md.start_of_burst=True ## is true when it is the first packet in a chain
         tx_md.end_of_burst=False ## is true when it's the last packet in a chain
-        tx_md.has_time_spec=True 
-        gps_time = self.uhd.types.TimeSpec(self.usrp.get_mboard_sensor("gps_time", 0).to_int() + 1)
-        tx_md.time_spec=gps_time ## when to send the first sample
+        tx_md.has_time_spec=False 
+        # gps_time = self.uhd.types.TimeSpec(self.usrp.get_mboard_sensor("gps_time", 0).to_int() + 1)
+        # tx_md.time_spec=gps_time ## when to send the first sample
+        logger.debug("num_buffs_to_send".format(num_buffs))
 
         ## transmit
         for i in range(num_buffs-1):
             samps_sent = tx_stream.send(big_buff, tx_md)
             tx_md.start_of_burst=False
+            logger.debug(".")
         tx_md.end_of_burst=True
         samps_sent = tx_stream.send(big_buff, tx_md)
+        logger.debug("TX over")
         return data
